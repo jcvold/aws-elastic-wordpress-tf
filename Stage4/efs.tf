7a31ab4519e4edc4857f6aef2bee5d6de50cafa5
John Vold <78934911+jcvold@users.noreply.github.com>
2023-02-27 10:01:17 -0600

    Adding efs configurations

--- Stage4/efs.tf
+++ Stage4/efs.tf
@@ -3,8 +3,22 @@ resource "aws_efs_file_system" "wordpress" {
   performance_mode = "generalPurpose"
   throughput_mode  = "bursting"
 
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+
   tags = {
     Name = "A4L-WORDPRESS-CONTENT"
   }
 }
 
+resource "aws_efs_access_point" "wordpress" {
+  file_system_id = aws_efs_file_system.wordpress.id
+}
+
+resource "aws_efs_mount_target" "wordpress" {
+  file_system_id  = aws_efs_file_system.wordpress.id
+  count           = length(aws_subnet.app_private_subnets.*.id)
+  subnet_id       = element(aws_subnet.app_private_subnets.*.id, count.index)
+  security_groups = [aws_security_group.efs_sg.id]
+}
