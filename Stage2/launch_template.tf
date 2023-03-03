f1dc206b637c1aacf6c368ab0e0c13c32b77d5a5
John Vold <78934911+jcvold@users.noreply.github.com>
2023-03-03 15:55:46 -0600

    Adding metadata info

--- Stage2/launch_template.tf
+++ Stage2/launch_template.tf
@@ -15,6 +15,11 @@ resource "aws_launch_template" "wordpress" {
 
   instance_type = "t2.micro"
 
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
   monitoring {
     enabled = false
   }
